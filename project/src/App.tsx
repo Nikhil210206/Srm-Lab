a71ffd2e140be4e27923de15459ed37791b18cc7
madhan <dmadhan692@gmail.com>
2025-02-03 21:02:43 +0530

    Removes backend and adds new test pages
    Deletes backend-related files including configuration, models, and routes
    Introduces CreateTest and TestInterface pages to the frontend

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
@@ -6,6 +5,8 @@ import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { StudentDashboard } from './pages/StudentDashboard';
 import { TeacherDashboard } from './pages/TeacherDashboard';
+import { CreateTest } from './pages/CreateTest';
+import TestInterface from './pages/TestInterface'; // Default import
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/student-dashboard" element={<StudentDashboard />} />
             <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
+            <Route path="/create-test" element={<CreateTest />} />
+            <Route path="/test-interface" element={<TestInterface />} /> {/* Use the component */}
           </Routes>
         </main>
         <Footer />
